d24b8be65c95c57bb48651581529b072bccfea4f
default <Adrina.peighambarzadeh@stu.ocsb.ca>
2025-04-17 16:27:05 +0000

    init

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
   <meta charset="utf-8" />
-  <meta name="description" content="Convert Fahrenheit to Celsius, PHP" />
-  <meta name="keywords" content="math, volume, PHP, sphere" />
-  <meta name="author" content="Adrina Peighambarzadeh" />
+  <meta name="description" content="The Random Number guess, with PHP" />
+  <meta name="keywords" content="mths, icd2o" />
+  <meta name="author" content="Adrina peighambarzadeh" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="./css/style.css" />
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css" />
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.red-orange.min.css" />
   <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png" />
   <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
   <link rel="manifest" href="site.webmanifest" />
-  <title>Convert Fahrenheit to Celsius, PHP</title>
+  <title>The Random Number guess, with PHP</title>
 </head>
 
 <body>
@@ -22,31 +22,30 @@
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">Convert Fahrenheit to Celsius, PHP</span>
+        <span class="mdl-layout-title">The Random Number guess, with PHP</span>
       </div>
     </header>
     <main class="mdl-layout__content">
       <div class="right-image">
-        <img src="./images/image (1).png" alt="temptaure pic" width=250px>
+        <img src="./images/dice.gif" alt="Dice gif image" width="500" />
       </div>
-      <br>
-      <div class="page-content">
-        <div class="page-content-heading">formula:(32°F − 32) ×5/9 = 0°C</div>
-        <p>Please enter indgree in Fahrenheit:</p>
+      <br />
+      <div class="page-content-guide">Please enter indgree between 1 to 6:</div>
+      <div class="page-content-php">
+        <form action="./answer.php" method="GET">
+          <div class="mdl-textfield mdl-js-textfield">
+            <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" name="user-number" />
+            <label class="mdl-textfield__label" for="user-number">Number between 1 to 6</label>
+            <span class="mdl-textfield__error">Input is not a number!</span>
+          </div>
+          <br />
+          <!-- Accent-colored raised button with ripple -->
+          <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
+            type="submit">
+            Check
+          </button>
+        </form>
       </div>
-      <label class="page-content">Dgree in Fahrenheit: </label>
-      <form action="answer.php" method="GET">
-        <div class="mdl-textfield mdl-js-textfield">
-          <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" name="dgree-in-fahrenheit">
-          <label class="mdl-textfield__label" for="hours-worked">dgree in Fahrenheit(°F)</label>
-          <span class="mdl-textfield__error">Input is not a number!</span>
-        </div>
-        <br>
-        <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" type="submit">
-          Calculate
-        </button>
-      </form>
-      <br>
     </main>
   </div>
 </body>
